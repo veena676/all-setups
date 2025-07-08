fac8b636eadd1bb42af9294bbe9fc0ba2b17ed03
shaikmustafa77 <awsanddevops37@gmail.com>
2025-07-08 16:19:08 +0530

    Create network-policy-minikube.sh

---
+++ network-policy-minikube.sh
@@ -0,0 +1,17 @@
+#make sure to run this script on  non-root user (ec2-user)
+#vim .bashrc
+#export PATH=$PATH:/usr/local/bin
+#source .bashrc
+
+yum install docker -y
+systemctl start docker
+systemctl status docker
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo mv kubectl /usr/local/bin/kubectl
+sudo chmod +x /usr/local/bin/kubectl
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+sudo yum install iptables -y
+yum install conntrack -y
+minikube start --driver=calico --force
+minikube status 
