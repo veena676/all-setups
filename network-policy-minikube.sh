42c0b5848160a2d56e1fa8a2e1f68096d901fc41
shaikmustafa77 <awsanddevops37@gmail.com>
2025-07-08 21:18:57 +0530

    Update network-policy-minikube.sh

--- network-policy-minikube.sh
+++ network-policy-minikube.sh
@@ -3,15 +3,18 @@
 #export PATH=$PATH:/usr/local/bin
 #source .bashrc
 
-yum install docker -y
-systemctl start docker
-systemctl status docker
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo yum install docker -y
+sudo systemctl start docker
+sudo systemctl status docker
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo mv kubectl /usr/local/bin/kubectl
 sudo chmod +x /usr/local/bin/kubectl
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 sudo yum install iptables -y
-yum install conntrack -y
+sudo yum install conntrack -y
+sudo chmod +x /var/run/docker.sock 
+sudo usermod -aG docker $USER && newgrp docker
+
 minikube start --driver=calico --force
 minikube status 
